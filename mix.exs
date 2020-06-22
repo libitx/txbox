5ce972ec9147b3951457cbed0b56d0b74a216931
libitx <libitx@protonmail.com>
2020-06-22 15:13:25 +0100

    version bump.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Txbox.MixProject do
   def project do
     [
       app: :txbox,
-      version: "0.1.0-beta.4",
+      version: "0.1.0",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
